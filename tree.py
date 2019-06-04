dd4361faa4a3a7c2f91ca1dd830cda76f42df998
packetscaper <packetscaper@gmail.com>
2019-06-04 21:59:16 +0000

    tree

---
+++ tree.py
@@ -0,0 +1,11 @@
+
+class Node():
+
+    def __init__(self,left_node=None,right_node=None,data=None):
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
+
+
+
+
